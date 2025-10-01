011d7976bd12cdc6da9b31c42d9dd25d97cac9b8
fundamentalpriciple <princedixit931@gmail.com>
2025-10-01 09:46:38 +0530

    some more practice code

---
+++ arrayCLI/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] array1 = new string[9];
+array1[0]="hello";
+array1[1]="world";
+array1[2]="bye";
+array1[3]="world";
+array1[4]="good morning";
+array1[5]="world";
+
+foreach(string greeting in array1) {
+    Console.WriteLine(greeting);
+}
+
+Console.WriteLine("the end");
